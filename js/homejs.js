da7a37d09058a9e5cbed3fc8937e6826077877ee
alimakh <amakhlouf@hawk.iit.edu>
2023-11-14 00:25:20 -0600

    fixing homejs.js file issues

--- js/homejs.js
+++ js/homejs.js
@@ -1,29 +1,51 @@
+/* Add to homejs.js */
+
 document.addEventListener('DOMContentLoaded', function() {
     const images = [
-        { src: 'images/image1.jpg', caption: '' }, 
-        { src: 'images/image2.jpg', caption: '' },
-        { src: 'images/image3.jpg', caption: '' }
+        { src: 'images/image1.jpg', caption: 'Caption 1' }, 
+        { src: 'images/image2.jpg', caption: 'Caption 2' },
+        { src: 'images/image3.jpg', caption: 'Caption 3' }
     ];
+    let currentImageIdx = 0;
     const carouselContainer = document.getElementById('carousel');
 
-    images.forEach((image, index) => {
+    function createImageElement(image, idx) {
         const imgElement = document.createElement('img');
         imgElement.src = image.src;
-        imgElement.alt = `Carousel image ${index + 1}`;
+        imgElement.alt = `Carousel image ${idx + 1}`;
         imgElement.className = 'carousel-image';
-        if (index === 0) imgElement.style.display = 'block'; 
+        imgElement.style.display = 'none'; // Images are hidden by default
+        if (idx === 0) {
+            imgElement.classList.add('visible'); // Show the first image
+        }
         carouselContainer.appendChild(imgElement);
-    });
+    }
 
-    
+    images.forEach((image, index) => createImageElement(image, index));
+
+    function moveToNextImage() {
+        images[currentImageIdx].classList.remove('visible');
+        currentImageIdx = (currentImageIdx + 1) % images.length;
+        images[currentImageIdx].classList.add('visible');
+    }
+
+    function moveToPreviousImage() {
+        images[currentImageIdx].classList.remove('visible');
+        currentImageIdx = (currentImageIdx - 1 + images.length) % images.length;
+        images[currentImageIdx].classList.add('visible');
+    }
+
+    // Setup event listeners for controls
+    const nextButton = document.createElement('div');
+    nextButton.textContent = '>';
+    nextButton.className = 'carousel-control right';
+    nextButton.onclick = moveToNextImage;
+    carouselContainer.appendChild(nextButton);
+
+    const prevButton = document.createElement('div');
+    prevButton.textContent = '<';
+    prevButton.className = 'carousel-control left';
+    prevButton.onclick = moveToPreviousImage;
+    carouselContainer.appendChild(prevButton);
 });
 
-const navLinks = document.querySelectorAll('nav ul li a');
-navLinks.forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        link.style.opacity = '0.7';
-    });
-    link.addEventListener('mouseleave', () => {
-        link.style.opacity = '1';
-    });
-});
