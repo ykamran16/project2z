ee1a99ff3c44812c5f673eb6753d4a3d144a60c5
alimakh <amakhlouf@hawk.iit.edu>
2023-11-15 18:07:15 -0600

    fix images not appearing

--- js/homejs.js
+++ js/homejs.js
@@ -1,41 +1,45 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const images = [
-        { src: 'images/image1.jpg', caption: '1' }, 
+        { src: 'images/image1.jpg', caption: '1' },
         { src: 'images/image2.jpg', caption: '2' },
         { src: 'images/image3.jpg', caption: '3' }
     ];
     let currentImageIdx = 0;
     const carouselContainer = document.getElementById('carousel');
+    const imgElements = []; 
 
     function createImageElement(image, idx) {
         const imgElement = document.createElement('img');
         imgElement.src = image.src;
         imgElement.alt = `Carousel image ${idx + 1}`;
         imgElement.className = 'carousel-image';
-        imgElement.style.display = 'none'; 
         if (idx === 0) {
-            imgElement.classList.add('visible'); 
+            imgElement.classList.add('visible');
+        } else {
+            imgElement.classList.add('hidden'); 
         }
+        imgElements.push(imgElement); 
         carouselContainer.appendChild(imgElement);
     }
 
-    images.forEach((image, index) => createImageElement(image, index));
+    images.forEach(createImageElement);
 
     function moveToNextImage() {
-        images[currentImageIdx].classList.remove('visible');
+        imgElements[currentImageIdx].classList.remove('visible');
+        imgElements[currentImageIdx].classList.add('hidden');
         currentImageIdx = (currentImageIdx + 1) % images.length;
-        images[currentImageIdx].classList.add('visible');
+        imgElements[currentImageIdx].classList.add('visible');
+        imgElements[currentImageIdx].classList.remove('hidden');
     }
 
     function moveToPreviousImage() {
-        images[currentImageIdx].classList.remove('visible');
+        imgElements[currentImageIdx].classList.remove('visible');
+        imgElements[currentImageIdx].classList.add('hidden');
         currentImageIdx = (currentImageIdx - 1 + images.length) % images.length;
-        images[currentImageIdx].classList.add('visible');
+        imgElements[currentImageIdx].classList.add('visible');
+        imgElements[currentImageIdx].classList.remove('hidden');
     }
 
-  
     const nextButton = document.createElement('div');
     nextButton.textContent = '>';
     nextButton.className = 'carousel-control right';
